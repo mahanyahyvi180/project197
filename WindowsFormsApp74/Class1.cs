734caecec8234f7fb87ae11f16fb395b4b41238f
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-12 10:41:13 +0330

    Add files via upload

---
+++ WindowsFormsApp74/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp74
+{
+    class cValid
+    {
+     public int lenght
+        {
+            get;
+            set;
+        }
+
+        private string ct;
+        public string Text
+        {
+            get
+            {
+                return ct;
+            }
+
+            set
+            {
+                if (value.Length <= lenght)
+                {
+                    char[] c;
+                    c = value.ToCharArray();
+                    string temp = "";
+                    foreach (char t in c)
+                    {
+                        if (char.IsLetter(t))
+                            temp = temp + t.ToString();
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ WindowsFormsApp74/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp74
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cValid t = new cValid();
+            t.lenght = int.Parse(textBox1.Text);
+            t.Text = textBox1.Text;
+            label1.Text = t.Text;
+        }
+    }
+}
